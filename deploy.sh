383f019e861e3a217d9e97dee7f030d2b5ea8e75
alexander-laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-01-19 14:19:07 -0500

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
-docker build -t alexanderlaudino/multi-client -f ./client/Dockerfile ./client
-docker build -t alexanderlaudino/multi-worker -f ./worker/Dockerfile ./worker
-docker built -t alexanderlaudino/multi-server -f ./server/Dockerfile ./server
-docker push alexanderlaudino/multi-client
-docker push alexanderlaudino/multi-worker
-docker push alexanderlaudino/multi-server
+docker build -t alexanderlaudino/multi-client:latest -t alexanderlaudino/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t alexanderlaudino/multi-worker:latest -t alexanderlaudino/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker built -t alexanderlaudino/multi-server:latest -t alexanderlaudino/multi-server:$SHA -f ./server/Dockerfile ./server
+
+docker push alexanderlaudino/multi-client:latest
+docker push alexanderlaudino/multi-worker:latest
+docker push alexanderlaudino/multi-server:latest
+
+docker push alexanderlaudino/multi-client:$SHA
+docker push alexanderlaudino/multi-worker:$SHA
+docker push alexanderlaudino/multi-server:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=alexanderlaudino/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=alexanderlaudino/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=alexanderlaudino/multi-server:$SHA
